619580434bb1dbaa6900531e42f7f805bbfc11fd
wonderwarrior <cahall555@gmail.com>
2019-09-26 21:00:02 -0600

    form

---
+++ php/laravel/resources/views/form.blade.php
@@ -0,0 +1,35 @@
+<html>
+<head>
+    <title>Test</title>
+</head>
+<body>
+    <h1>Form</h1>
+    <form method="post">
+        <div>
+            <label for="username">Username:</label>
+            <input type="text" id="username" name="username">
+        </div>
+        <div>
+            <label for="password">Password:</label>
+            <input type="text" id="password" name="password">
+        </div>
+        <div>
+            <label for="server">Server:</label>
+            <input type="text" id="server" name="server">
+        </div>
+        <div>
+            <label for="database">Database:</label>
+            <input type="text" id="database" name="database">
+        </div>
+        <div>
+            <label for="propert_id">Property ID:</label>
+            <input type="text" id="property_id" name="property_id">
+        </div>
+        <div>
+            <label for="endpoint">Endpoint:</label>
+            <input type="text" id="endpoint" name="endpoint">
+        </div>
+    </form>
+    
+</body>
+</html>
